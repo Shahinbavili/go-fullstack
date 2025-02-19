82a0b209ee0ca01707b5b0c6c9d78c9252081bdb
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2025-02-19 11:58:15 +0100

    CRUD operations

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const app = express();
+const bodyParser = require("express");
 const username = encodeURIComponent("shahinbavili");
 const password = encodeURIComponent("sqh88BP4K4dUtVv");
 const uri = `mongodb+srv://${username}:${password}@cluster0.yns2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const mongoose = require('mongoose');
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const Thing = require('./models/Thing');
+
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(err => console.error('Connexion à MongoDB échouée !', err));
 
-
+const app = express();
 
 app.use(express.json());
 
@@ -21,33 +24,40 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.json());
+
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet crée !',
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({error}));
 });
 
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json('Objet modifié !'))
+        .catch(error => res.status(400).json({error}));
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json('Objet supprimé !'))
+        .catch(error => res.status(400).json({error}));
+})
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(400).json({error}));
+})
+
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app;
